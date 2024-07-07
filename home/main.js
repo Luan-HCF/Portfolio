683b0dcd489cdfd3581a0b99fce0b547b0d09565
Luan-HCF <alucardsantosxp@gmail.com>
2024-07-07 07:22:38 -0300

    update

--- home/main.js
+++ home/main.js
@@ -1,20 +1,22 @@
+// Função para alternar a visibilidade do menu
 function toggleMenu() {
-                var menu = document.getElementById("menuDropdown");
-                if (menu.style.display === "block") {
-                    menu.style.display = "none";
-                } else {
-                    menu.style.display = "block";
-                }
+    var menu = document.getElementById("menuDropdown");
+    if (menu.style.display === "block") {
+        menu.style.display = "none"; // Esconde o menu se estiver visível
+    } else {
+        menu.style.display = "block"; // Mostra o menu se estiver oculto
+    }
+}
+
+// Evento que fecha o menu ao clicar fora dele
+window.onclick = function(event) {
+    if (!event.target.matches('.menu-button')) { // Verifica se o elemento clicado não é um botão do menu
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") { // Verifica se o menu está visível
+                openDropdown.style.display = "none"; // Esconde o menu
             }
-            
-            window.onclick = function(event) {
-                if (!event.target.matches('.menu-button')) {
-                    var dropdowns = document.getElementsByClassName("dropdown-content");
-                    for (var i = 0; i < dropdowns.length; i++) {
-                        var openDropdown = dropdowns[i];
-                        if (openDropdown.style.display === "block") {
-                            openDropdown.style.display = "none";
-                        }
-                    }
-                }
-            }
+        }
+    }
+}
